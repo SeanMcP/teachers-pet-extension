e39ad466165b755484f4c8ab03d8aeb6383ab003
Sean McPherson <smcpherson@niche.com>
2021-03-29 09:34:32 -0400

    fix: routing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 module.exports = (config) => {
   config.addPassthroughCopy({ "src/_includes/styles.css": "styles.css" });
 
+  /**
+   * Example: {% link 'group-maker/' %}Link to group maker{% endlink %}
+   */
+  config.addPairedShortcode(
+    "link",
+    (content, page) =>
+      `<a href="/build/${page ? page : ""}index.html">${content}</a>`
+  );
+
   return {
     dir: {
       input: "src",
